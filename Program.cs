4f27e593c2d881586c7b4b59ba641190041bd9d2
pkid1729 <pkid1729@gmail.com>
2018-09-08 01:07:04 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Number = new int[4];
+            Number[0] = 987;
+            Number[1] = 654;
+            Number[2] = 5451;
+            Number[3] = 5645;
+            foreach (int k in Number)
+
+            {
+                Console.WriteLine(k);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
